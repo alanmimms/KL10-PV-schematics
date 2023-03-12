dbecfd53944fa7b5a965ac948eb5a3f6880ba85c
Alan Mimms <alanmimms@gmail.com>
2023-03-12 16:01:07 -0700

    Nearly done switching to <ABn> backplane labels

---
+++ get-backplane.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const fs = require('fs');
+
+const RE = /"(?<net>[^"<]*)\<(?<pin>[A-F][A-Z][12])\>"/;
+const fileNames = 'DP01,DP02,DP03,DP04'.split(',').map(f => `${f}.kicad_sch`);
+const nets = {};
+
+
+fileNames.forEach(fileName => {
+
+  const content = fs.readFileSync(fileName, 'utf-8');
+
+  content.split(/\n/).forEach(line => {
+    const m = line.match(RE);
+    if (!m) return;
+
+    let {net, pin} = m.groups;
+    if (!net) return;
+    net = net.trim();
+
+    if (nets[pin] && nets[pin] != net) {
+      console.error(`Pin <${pin}> defined with net "${nets[pin]}" and net "${net}"`);
+    }
+
+    nets[pin] = net;
+  });
+});
+
+
+console.log(Object.keys(nets).sort(pinSort).map(k => `${k}: ${nets[k]}`).join('\n'));
+
+
+function pinSort(p1, p2) {
+  const s1 = p1[0].concat(p1[2], p1[1]);
+  const s2 = p2[0].concat(p2[2], p2[1]);
+  return s1 > s2 ? 1 : (s1 == s2 ? 0 : -1);
+}
